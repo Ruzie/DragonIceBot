19fad04ab95ae2fbbf95d9411ebab1b51f06f2cc
Ahflow <luhamailbox@gmail.com>
2022-03-18 10:23:39 -0700

    Add files via upload

--- src/Utils/Emojis.ts
+++ src/Utils/Emojis.ts
@@ -2,4 +2,24 @@ export default class Emojis {
     static get noice(): string {
         return "<:noice:913864505593823272>";
     }
+
+    static get error(): string {
+        return "<:hyperthink:913864534471610368>";
+    }
+
+    static get ahsad(): string {
+        return "<:SadCat:913864830266527785>";
+    }
+
+    static get startmusic(): string {
+        return "<:coolthonk:913864333958721589>";
+    }
+
+    static get loading(): string {
+        return "<:10pcuckfish:913864006094168084>";
+    }
+
+    static get added(): string {
+        return "<:pepeyep:913864664381812736>";
+    }
 }
