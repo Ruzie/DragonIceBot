3edb95199a41d0b0dd482d11cc8c157a7c416deb
Ahflow <luhamailbox@gmail.com>
2022-03-18 10:22:46 -0700

    Add files via upload

--- src/Manager/InteractionManager.ts
+++ src/Manager/InteractionManager.ts
@@ -46,27 +46,26 @@ export default class SlashManager extends EventEmitter {
                 Logger.preload(`${this.constructor.name} Command '${Command.name}' loaded (@${Command.name})`);
             }
         }
-        Logger.preload(`${this.constructor.name}: Loaded ${this.commands.size} interaction client command(s)`);
+        Logger.preload(`${this.constructor.name}: Total ${this.commands.size} interaction client command(s)`);
         this.built = true;
         return this;
     }
 
     public register(guildID?: string): void {
         const commands = this.commands.map((cmd: any) => cmd.interactionData);
-        console.log(commands);
         if (guildID) {
             this.client.bulkEditGuildCommands(guildID, commands);
             Logger.success(`${this.constructor.name}: Registered guild commands.`);
         } else {
             this.client.bulkEditCommands(commands);
-            Logger.success(`${this.constructor.name}: Registered global commands.`);
+            Logger.success(`${this.constructor.name}: Registering global commands, it shall take 1 hour to sync with all guilds.`);
         }
     }
 
     public async exec(interaction: any) {
         try {
             const command = this.commands.get(interaction.data.name);
-            Logger.success(`${this.constructor.name}: Executing command ${command.name} (@${command.uid})`);
+            Logger.success(`${this.constructor.name}: Executing command ${command.name}`);
             await command.run({ interaction });
         } catch (error) {
             Logger.error(error);
